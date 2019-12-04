94ee9ac2fc6aedf6034c4eacb0da1ceb33e4bb6c
Reggie Baker <rbaker@vinsuite.com>
2019-12-04 15:25:24 -0800

    add namespace

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+# from django.template import loader
+
 from .models import Question
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
+    # looks like this is pulling from DB?
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]    
+    context = {
+        'latest_questions_list': latest_question_list,
+    }
 
-    return HttpResponse(output)
-    # return HttpResponse('Howdy world. This is the polls index.')
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s." % question_id)
+    """
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does no exist")
+    """
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %s"
